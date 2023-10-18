c11b79cfe41c348623578f08318c07dade07e8db
Mark Duffy <jazz_mavericks@hotmail.com>
2023-10-18 12:17:50 +0100

    updating subclasses

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-
-
 class Cyberpet {
     constructor() {
         this.happiness = 50;
@@ -37,6 +35,47 @@ class Cyberpet {
         }
     }
 };
+
+class Wookie extends Cyberpet {
+    // Add specific properties and methods for Wookie cyberpet here
+}
+
+class Porg extends Cyberpet {
+    // Add specific properties and methods for Porg cyberpet here
+}
+
+class Ewok extends Cyberpet {
+    // Add specific properties and methods for Ewok cyberpet here
+}
+
+class Rancor extends Cyberpet {
+    constructor() {
+        super(); // Call the constructor of the base class
+        this.roarLevel = 50;
+    }
+
+    // Custom method for the Rancor cyberpet
+    roar() {
+        if (this.roarLevel < 100) {
+            this.roarLevel += 10;
+            this.updateUI();
+        }
+    }
+
+    // Override the decreaseStats method to make the Rancor more resilient
+    decreaseStats() {
+        this.hunger += 1;
+        this.thirst += 1;
+        this.happiness -= 2;
+        this.roarLevel -= 1;
+    }
+
+    // Override the updateUI method to display the roar level
+    updateUI() {
+        super.updateUI(); // Call the base class updateUI method
+        document.querySelector(".roar-level").textContent = this.roarLevel;
+    }
+}
 
 const pet = new Cyberpet();
 
