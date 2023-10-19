383aa5960f0308dac8c1c83d387c03327c9654f4
Mark Duffy <jazz_mavericks@hotmail.com>
2023-10-19 12:21:56 +0100

    added restart button and amended css

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,11 @@
 const petImages = document.querySelectorAll(".pet-image");
+
+// Define variables for specific buttons
+const roarBtn = document.querySelector(".roar-btn");
+const squawkBtn = document.querySelector(".squawk-btn");
+const drumBtn = document.querySelector(".drum-btn");
+const chuckleBtn = document.querySelector(".chuckle-btn");
+
 petImages.forEach(image => {
     image.addEventListener("click", () => {
         const petType = image.getAttribute("data-pet-type");
@@ -8,15 +15,39 @@ petImages.forEach(image => {
         if (petType === "Wookie") {
             selectedPet = new Wookie();
             showSpecificAttribute("chuckleLevel", selectedPet.chuckleLevel);
+            // Show the "chuckle" button for Wookie
+            chuckleBtn.style.display = "block";
+            // Hide other pet-specific buttons
+            roarBtn.style.display = "none";
+            squawkBtn.style.display = "none";
+            drumBtn.style.display = "none";
         } else if (petType === "Porg") {
             selectedPet = new Porg();
             showSpecificAttribute("squawkLevel", selectedPet.squawkLevel);
+            // Show the "squawk" button for Porg
+            squawkBtn.style.display = "block";
+            // Hide other pet-specific buttons
+            roarBtn.style.display = "none";
+            chuckleBtn.style.display = "none";
+            drumBtn.style.display = "none";
         } else if (petType === "Ewok") {
             selectedPet = new Ewok();
-            showSpecificAttribute("cuddlyLevel", selectedPet.cuddlyLevel);
+            showSpecificAttribute("drumLevel", selectedPet.drumLevel);
+            // Show the "drum" button for Ewok
+            drumBtn.style.display = "block";
+            // Hide other pet-specific buttons
+            roarBtn.style.display = "none";
+            squawkBtn.style.display = "none";
+            chuckleBtn.style.display = "none";
         } else if (petType === "Rancor") {
             selectedPet = new Rancor();
             showSpecificAttribute("roarLevel", selectedPet.roarLevel);
+            // Show the "roar" button for Rancor
+            roarBtn.style.display = "block";
+            // Hide other pet-specific buttons
+            squawkBtn.style.display = "none";
+            chuckleBtn.style.display = "none";
+            drumBtn.style.display = "none";
         } else {
             // Handle unsupported or default selection
             alert("Please select a valid cyberpet type.");
@@ -24,6 +55,8 @@ petImages.forEach(image => {
         }
 
         // Hide the pet selection menu and display the game interface
+        document.querySelector(".choosetext").style.display = "none";
+        document.querySelector(".bigsmalltxt1").style.display = "none";
         document.querySelector(".pet-selection").style.display = "none";
         document.querySelector(".game-interface").style.display = "block";
 
@@ -31,6 +64,7 @@ petImages.forEach(image => {
         startGame(selectedPet);
     });
 });
+
 
 // Function to display the appropriate attributes for the selected pet
 function showSpecificAttribute(attributeName, attributeValue) {
@@ -85,14 +119,14 @@ class Cyberpet {
 
 class Wookie extends Cyberpet {
     constructor() {
-        super();; // Call the constructor of the basic class (hunger, happiness, thirst), and add the following Wookie specific attribute
+        super(); // Call the constructor of the basic class (hunger, happiness, thirst), and add the following Wookie specific attribute
         this.chuckleLevel = 150;
     }
 
     // Custom method for the Wookie cyberpet
     chuckle() {
         if (this.chuckleLevel < 300) {
-            this.chuckleLevel -= 30;
+            this.chuckleLevel += 30;
             this.updateUI();
         }
     }
@@ -114,7 +148,7 @@ class Wookie extends Cyberpet {
 
 class Porg extends Cyberpet {
     constructor() {
-        super();; // Call the constructor of the basic class (hunger, happiness, thirst), and add the following Porg specific attribute
+        super(); // Call the constructor of the basic class (hunger, happiness, thirst), and add the following Porg specific attribute
         this.squawkLevel = 150;
     }
 
@@ -131,7 +165,7 @@ class Porg extends Cyberpet {
         this.hunger += 3;
         this.thirst += 3;
         this.happiness -= 6;
-        this.cuddlyLevel -= 3;
+        this.squawkLevel -= 3;
     }
 
     // Override the updateUI method to display the squawk level
@@ -143,14 +177,14 @@ class Porg extends Cyberpet {
 
 class Ewok extends Cyberpet {
     constructor() {
-        super();; // Call the constructor of the basic class (hunger, happiness, thirst), and add the following Ewok specific attribute
-        this.cuddlyLevel = 150;
+        super(); // Call the constructor of the basic class (hunger, happiness, thirst), and add the following Ewok specific attribute
+        this.drumLevel = 150;
     }
 
     // Custom method for the Ewok cyberpet
-    cuddly() {
-        if (this.cuddlyLevel < 300) {
-            this.cuddlyLevel += 30;
+    drum() {
+        if (this.drumLevel < 300) {
+            this.drumLevel += 30;
             this.updateUI();
         }
     }
@@ -160,19 +194,19 @@ class Ewok extends Cyberpet {
         this.hunger += 3;
         this.thirst += 3;
         this.happiness -= 6;
-        this.cuddlyLevel -= 3;
+        this.drumLevel -= 3;
     }
 
-    // Override the updateUI method to display the cuddly level
+    // Override the updateUI method to display the drum level
     updateUI() {
         super.updateUI(); // Call the basic class updateUI method, and add the following Ewok specific update
-        document.querySelector(".cuddlyLevel").style.width = `${this.cuddlyLevel}px`
+        document.querySelector(".drumLevel").style.width = `${this.drumLevel}px`
     }
 }
 
 class Rancor extends Cyberpet {
     constructor() {
-        super();; // Call the constructor of the basic class (hunger, happiness, thirst), and add the following Rancor specific attribute
+        super(); // Call the constructor of the basic class (hunger, happiness, thirst), and add the following Rancor specific attribute
         this.roarLevel = 150;
     }
 
@@ -197,10 +231,13 @@ class Rancor extends Cyberpet {
         super.updateUI(); // Call the basic class updateUI method, and add the following Rancor specific update
         document.querySelector(".roarLevel").style.width = `${this.roarLevel}px`
     }
+
 }
 
+// Set up a new pet
 const pet = new Cyberpet();
 
+// Set up the event listeners for the gameplay buttons
 document.querySelector(".eat-btn").addEventListener("click", () => {
     pet.eat();
 })
@@ -213,7 +250,41 @@ document.querySelector(".play-btn").addEventListener("click", () => {
     pet.play();
 })
 
+document.querySelector(".chuckle-btn").addEventListener("click", () => {
+    if (selectedPet instanceof Wookie) {
+        selectedPet.chuckle();
+    }
+});
+
+document.querySelector(".squawk-btn").addEventListener("click", () => {
+    if (selectedPet instanceof Porg) {
+        selectedPet.squawk();
+    }
+});
+
+document.querySelector(".drum-btn").addEventListener("click", () => {
+    if (selectedPet instanceof Ewok) {
+        selectedPet.drum();
+    }
+});
+
+document.querySelector(".roar-btn").addEventListener("click", () => {
+    if (selectedPet instanceof Rancor) {
+        selectedPet.roar();
+    }
+});
+
+
 setInterval(() => {
     pet.decreaseStats();
     pet.updateUI();
 }, 2000);
+
+document.querySelector(".restart-btn").addEventListener("click", () => {
+    selectedPet = null;
+    // Show the pet selection images
+    petImages.forEach(image => {image.style.display = "block"; console.log("Image displayed:", image);});
+    document.querySelector(".choosetext").style.display = "block"; // Show the select a pet text
+    document.querySelector(".bigsmalltxt1").style.display = "block"; // Show the instructions
+    document.querySelector(".game-interface").style.display = "none"; // Do not show the game play section
+});
